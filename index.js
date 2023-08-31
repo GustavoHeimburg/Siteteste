3009588ff30949361bd0cdf6f812f439f6549159
GustavoHeimburg <gutogustavogu@gmail.com>
2023-08-30 23:42:46 -0300

    Refatorando

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 window.alert("Seja bem vindo ao nosso site");
 window.alert("Nosso site consiste em imagens graficas +18!");
-window.alert("A pagina tem conteudos porno!");
-window.alert("Caso ser menor de idade nao conseguirá entrar na pagina!");
-window.alert("Bom aproveito😊✨");
+window.confirm("Esta de acordo com o site?")
+window.alert("Ok,Bom aproveito😊✨");
