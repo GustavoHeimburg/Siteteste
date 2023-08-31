0ab02647b5c9dad0986e1cabd01e593876e2877b
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-08-30 21:42:57 -0300

    Mensagens de aviso!

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-window.alert("Olá Mundo!");
+window.alert("Seja bem vindo ao nosso site");
+window.alert("Nosso site consiste em imagens graficas +18!");
+window.alert("A pagina tem conteudos porno!");
+window.alert("Caso ser menor de idade nao conseguirá entrar na pagina!");
+window.alert("Bom aproveito😊✨");
